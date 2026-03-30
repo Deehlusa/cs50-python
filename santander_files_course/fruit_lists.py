33f00f1d0c5c7cd88a95598c2019808247688112
André Rodrigues <deeh.lusa@hotmail.com>
2026-03-30 18:42:04 +0100

    renamed folder name  for santander_files_course

---
+++ santander_files_course/fruit_lists.py
@@ -0,0 +1,19 @@
+fruit = ["Banana", "Apple", "Orange"]
+
+fruit.append("Pear")
+print(fruit)
+
+fruit.insert(0, "Grapefruit")
+print(fruit)
+
+fruit.remove("Banana")
+print(fruit)
+
+fruit_remove = fruit.pop(2)
+print(fruit_remove) # print organge fruit
+
+fruit.sort()
+print(fruit) # ascend order list
+
+fruit.reverse()
+print (fruit) # ascend reserver list
