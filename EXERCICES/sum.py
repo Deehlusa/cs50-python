bddf494b564b1f21f7455ec164a8f3540777b78b
André Rodrigues <deeh.lusa@hotmail.com>
2026-04-08 02:06:02 +0100

    using method .format to exibition result

--- EXERCICES/sum.py
+++ EXERCICES/sum.py
@@ -3,4 +3,6 @@ n2 = int(input("Digite outro valor: "))
 
 result = n1 + n2
 
-print("A soma entre:", n1, "+", n2, "=" ,result)
+#print("A soma entre:", n1, "+", n2, "=" ,result)
+# using method .format to exibition result
+print("A soma entre {} + {} = {}".format(n1, n2, result))
